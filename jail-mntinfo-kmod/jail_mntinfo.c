7918003990dbf14197c97058367c72252a8828cf
Bernard Buri <bernard.buri@seekda.com>
2025-04-08 05:42:34 +0200

    add missing check to avoid exposing fsid to unprivileged users

--- jail-mntinfo-kmod/jail_mntinfo.c
+++ jail-mntinfo-kmod/jail_mntinfo.c
@@ -1,8 +1,6 @@
 #include <sys/param.h>
 #include <sys/module.h>
 #include <sys/cdefs.h>
-
-#include <sys/param.h>
 #include <sys/types.h>
 #include <sys/kernel.h>
 #include <sys/systm.h>
@@ -34,16 +32,16 @@
 #include <sys/uuid.h>
 #include <sys/sbuf.h>
 
-static int jail_mntinfo_modevent(module_t mod, int type, void *unused) {
-    switch (type) {
-        case MOD_LOAD:
-            break;
-        case MOD_UNLOAD:
-            break;
-        default:
-            return EOPNOTSUPP;
-    }
-    return 0;
+static int jail_mntinfo_modevent(__unused module_t mod, int type, void *unused) {
+        switch (type) {
+                case MOD_LOAD:
+                        break;
+                case MOD_UNLOAD:
+                        break;
+                default:
+                        return EOPNOTSUPP;
+        }
+        return 0;
 }
 
 static void
@@ -89,46 +87,57 @@ encode_hex(struct sbuf *sb, unsigned i)
 }
 
 static void
+emit_fsid(struct sbuf *sb, struct fsid *fsid)
+{
+        sbuf_cat(sb, "\",\"fsid\":\"");
+        encode_hex(sb, (unsigned) fsid->val[0]);
+        encode_hex(sb, (unsigned) fsid->val[1]);
+}
+
+static void
 build_json(struct sbuf *sb, char *jpath, int enforce_statfs, struct thread *td)
 {
         struct mount *mp;
         int jpathlen = strlen(jpath);
         int is_first = 1;
+        int expose_fsid = 0;
+
+        expose_fsid = priv_check_cred(td->td_ucred, PRIV_VFS_UNMOUNT) == 0;
 
         sbuf_cat(sb, "{\"mounted\":[");
 
         mtx_lock(&mountlist_mtx);
         TAILQ_FOREACH(mp, &mountlist, mnt_list) {
                 if (vfs_suser(mp, td) == 0) {
-                    char *mntonname = mp->mnt_stat.f_mntonname;
+                        char *mntonname = mp->mnt_stat.f_mntonname;
 
-                    if (enforce_statfs > 0) {
-                            // remove jail path prefix
-                            size_t len = strlen(mntonname);
-                            if (jpathlen > 0 && len > jpathlen 
-                                && mntonname[jpathlen] == '/' 
-                                && strncmp(jpath, mntonname, jpathlen) == 0)
-                            {
-                                    mntonname = &mntonname[jpathlen];
-                            }
-                    }
+                        if (enforce_statfs > 0) {
+                                // remove jail path prefix
+                                size_t len = strlen(mntonname);
+                                if (jpathlen > 0 && len > jpathlen
+                                    && mntonname[jpathlen] == '/'
+                                    && strncmp(jpath, mntonname, jpathlen) == 0)
+                                {
+                                        mntonname = &mntonname[jpathlen];
+                                }
+                        }
 
-                    if (is_first) {
-                            is_first = 0;
-                    } else {
-                            sbuf_putc(sb, ',');
-                    }
+                        if (is_first) {
+                                is_first = 0;
+                        } else {
+                                sbuf_putc(sb, ',');
+                        }
 
-                    sbuf_cat(sb, "{\"fstype\":\"");
-                    escape_json(sb, mp->mnt_stat.f_fstypename);
-                    sbuf_cat(sb, "\",\"special\":\"");
-                    escape_json(sb, mp->mnt_stat.f_mntfromname);
-                    sbuf_cat(sb, "\",\"node\":\"");
-                    escape_json(sb, mntonname);
-                    sbuf_cat(sb, "\",\"fsid\":\"");
-                    encode_hex(sb, (unsigned) mp->mnt_stat.f_fsid.val[0]);
-                    encode_hex(sb, (unsigned) mp->mnt_stat.f_fsid.val[1]);
-                    sbuf_cat(sb, "\"}");
+                        sbuf_cat(sb, "{\"fstype\":\"");
+                        escape_json(sb, mp->mnt_stat.f_fstypename);
+                        sbuf_cat(sb, "\",\"special\":\"");
+                        escape_json(sb, mp->mnt_stat.f_mntfromname);
+                        sbuf_cat(sb, "\",\"node\":\"");
+                        escape_json(sb, mntonname);
+                        if (expose_fsid) {
+                                emit_fsid(sb, &mp->mnt_stat.f_fsid);
+                        }
+                        sbuf_cat(sb, "\"}");
                 }
         }
         mtx_unlock(&mountlist_mtx);
@@ -147,7 +156,7 @@ sysctl_mntinfojson(SYSCTL_HANDLER_ARGS)
         int error = 0;
         struct sbuf *sb;
 
-        if (req->td == NULL || req->td->td_ucred == 0) {
+        if (req->td == NULL || req->td->td_ucred == NULL) {
                 enforce_statfs = 2;
         } else if (req->td->td_ucred->cr_prison != NULL) {
                 struct prison *p = req->td->td_ucred->cr_prison;
@@ -156,19 +165,21 @@ sysctl_mntinfojson(SYSCTL_HANDLER_ARGS)
         }
 
         if (enforce_statfs > 1) {
-                char *empty = "[]";
-                sysctl_handle_string(oidp, empty, strlen(empty), req);
+                char *empty = "{\"mounted\":[]}";
+                return sysctl_handle_string(oidp, empty, strlen(empty), req);
         }
 
         if (req->oldptr == NULL) {
                 sb = sbuf_new(NULL, NULL, PAGE_SIZE, 
                               SBUF_FIXEDLEN | SBUF_INCLUDENUL);
+                KASSERT(sb != NULL, ("sbuf_new failed"));
                 sbuf_set_drain(sb, sbuf_count_drain, &len);
                 build_json(sb, jpath, enforce_statfs, req->td);
                 req->oldidx = hint = len;
         } else {
                 sb = sbuf_new(NULL, NULL, hint, 
                               SBUF_AUTOEXTEND | SBUF_INCLUDENUL);
+                KASSERT(sb != NULL, ("sbuf_new failed"));
                 build_json(sb, jpath, enforce_statfs, req->td);
                 hint = sbuf_len(sb);
                 error = SYSCTL_OUT(req, sbuf_data(sb), sbuf_len(sb));
@@ -179,14 +190,14 @@ sysctl_mntinfojson(SYSCTL_HANDLER_ARGS)
 
 SYSCTL_DECL(_security_jail);
 SYSCTL_PROC(_security_jail, OID_AUTO, mntinfojson,
-    CTLTYPE_STRING | CTLFLAG_RD | CTLFLAG_MPSAFE, 0, 0,
-    sysctl_mntinfojson, "",
-    "Get mount info as json");
+            CTLTYPE_STRING | CTLFLAG_RD | CTLFLAG_MPSAFE, 0, 0,
+            sysctl_mntinfojson, "",
+            "Get mount info as json");
 
 static moduledata_t jail_mntinfo_mod = {
-    "jail_mntinfo",
-    jail_mntinfo_modevent,
-    NULL
+        "jail_mntinfo",
+        jail_mntinfo_modevent,
+        NULL
 };
 
 DECLARE_MODULE(jail_mntinfo, jail_mntinfo_mod, SI_SUB_KLD, SI_ORDER_ANY);

e1eed95aaaa3a875a8a8dcc751b72b03689952a4
Bernard Buri <bernard.buri@seekda.com>
2025-04-11 19:56:57 +0200

    remove unused dependencies

--- oci-interceptor/build.gradle.kts
+++ oci-interceptor/build.gradle.kts
@@ -18,9 +18,5 @@ dependencies {
 
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
-
-    api("io.ktor:ktor-network:3.1.2")
-
     api("com.github.ajalt.clikt:clikt:5.0.3")
-    runtimeOnly("org.slf4j:slf4j-simple:2.0.13")
 }

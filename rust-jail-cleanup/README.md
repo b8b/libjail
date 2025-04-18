abb91f1bc495f157db17e80a0a179d8ad4e5fb90
Bernard Buri <bernard.buri@seekda.com>
2025-04-18 09:52:28 +0200

    implement attached cleanup in rust

---
+++ rust-jail-cleanup/README.md
@@ -0,0 +1,27 @@
+# rust-jail-cleanup
+
+## building
+
+    mkdir -p build-freebsd-`uname -r`-`uname -m`
+    
+    ./bin/jpkg -r build-freebsd-`uname -r`-`uname -m` \
+      --mount rust-jail-cleanup:/root \
+      --exec-stop "cd /root && cargo build --release" \
+      install -y \
+      rust FreeBSD-clang FreeBSD-lld FreeBSD-utilities \
+      FreeBSD-clang-dev FreeBSD-clibs-dev FreeBSD-runtime-dev \
+      FreeBSD-utilities-dev FreeBSD-libexecinfo-dev FreeBSD-rc
+
+## usage
+
+rust-jail-cleanup -j <JID> <COMMAND>
+
+Commands:
+mnt-info      print jail's mount info json
+unmount       attach to jail and call unmount(BY_FSID)
+destroy-vmm   attach to jail and destroy vmm (via sysctl)
+
+## roadmap
+
+(1) allow the jvm code to use this tool instead of attaching itself
+(2) port the complete `intercept-rcjail cleanup` command.
---
+++ rust-jail-cleanup/src/main.rs
@@ -0,0 +1,69 @@
+extern crate clap;
+extern crate jail;
+extern crate sysctl;
+extern crate libc;
+extern crate nix;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short)]
+    jid: String,
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    MntInfo,
+    Unmount {
+        #[arg(short, long)]
+        fsid: String,
+    },
+    DestroyVmm {
+        #[arg(short, long)]
+        name: String,
+    },
+}
+
+use jail::RunningJail;
+use sysctl::{Ctl, CtlValue, Sysctl};
+
+use std::ffi::CString;
+use libc::{c_char, c_int};
+use nix::errno::Errno;
+
+extern "C" {
+    fn unmount(dir: *const c_char, flags: c_int) -> c_int;
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    let running_jail = RunningJail::from_name(args.jid.as_str()).unwrap();
+    running_jail.attach().unwrap();
+
+    match args.command {
+        Commands::MntInfo => {
+            let ctl = Ctl::new("security.jail.mntinfojson").unwrap();
+            let val = ctl.value_string().unwrap();
+            println!("{}", val);
+        }
+        Commands::Unmount { fsid } => {
+            let dir = CString::new(fsid).expect("CString::new failed");
+            let flags = 0x0000000008000000; // MNT_BYFSID
+            let result = unsafe { unmount(dir.as_ptr(), flags) };
+            if result != 0 {
+                let e = Errno::last();
+                println!("Unmount failed with error code: {}", e);
+                std::process::exit(1);
+            }
+        }
+        Commands::DestroyVmm { name } => {
+            let ctl = Ctl::new("hw.vmm.destroy").unwrap();
+            let val = CtlValue::String(name);
+            ctl.set_value(val).unwrap();
+        }
+    }
+}
